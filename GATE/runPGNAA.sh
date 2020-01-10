38a32d94728f1ca11448dd81774d5241ee518833
Helge E. S. Pettersen <helge@krnmpi0.hvl.no>
2020-01-10 12:50:29 +0100

    Updated analysis

--- GATE/runPGNAA.sh
+++ GATE/runPGNAA.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-for i in `seq 1 1 40`; do
-   tsp "'[idx,$i']" Main.mac
+for i in `seq 0 1 80`; do
+   tsp Gate -a "'[idx,$i] [samplematerial,WaterWithSalt]'" Main.mac
+   tsp Gate -a "'[idx,$i] [samplematerial,Hydrogen]'" Main.mac
+   tsp Gate -a "'[idx,$i] [samplematerial,Oxygen]'" Main.mac
+   tsp Gate -a "'[idx,$i] [samplematerial,Strontium]'" Main.mac
+   tsp Gate -a "'[idx,$i] [samplematerial,Chlorine]'" Main.mac
+   tsp Gate -a "'[idx,$i] [samplematerial,Sodium]'" Main.mac
 done
